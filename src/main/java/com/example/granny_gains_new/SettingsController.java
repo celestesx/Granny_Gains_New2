58a01f0b87cd76622e6dba2de62a24591bed1512
Stellarium0-0 <echristina86@gmail.com>
2024-09-18 13:14:45 +1000

    Back Button

--- src/main/java/com/example/granny_gains_new/SettingsController.java
+++ src/main/java/com/example/granny_gains_new/SettingsController.java
@@ -1,14 +1,28 @@
 package com.example.granny_gains_new;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SettingsController {
 
+    @FXML
+    private Button backButton;
 
-
-
+    @FXML
+    protected void handleBackToHome() throws IOException {
+        Stage stage = (Stage) backButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("granny_gains_home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 650);
+        stage.setScene(scene);
+    }
 }
+
